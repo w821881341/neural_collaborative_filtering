c9107ab670e6e5b9eeefb2650b89416927765add
owenwei <821881341@qq.com>
2019-02-16 13:35:27 +0800

    add AutoDCF

--- newAutoDCF.py
+++ newAutoDCF.py
@@ -114,10 +114,10 @@ def get_model(train_matrix, num_users, num_items, layers=[20, 10], reg_layers=[0
     predict_result = predict_layer(vector)
 
     user_cost = Lambda(
-        lambda x: K.sum(K.square(x[0] - x[1]) * x[0], 1, keepdims=True) / (K.sum(x[0], 1, keepdims=True) + K.epsilon()),
+        lambda x: K.sum(K.square(x[0] - x[1]) * x[0], 1, keepdims=False) / (K.sum(x[0], 1, keepdims=False) + K.epsilon()),
         output_shape=(1,), name='user_reconstruct_cost')([user_data, user_decoder_MLP])
     item_cost = Lambda(
-        lambda x: K.sum(K.square(x[0] - x[1]) * x[0], 1, keepdims=True) / (K.sum(x[0], 1, keepdims=True) + K.epsilon()),
+        lambda x: K.sum(K.square(x[0] - x[1]) * x[0], 1, keepdims=False) / (K.sum(x[0], 1, keepdims=False) + K.epsilon()),
         output_shape=(1,), name='item_reconstruct_cost')([item_data, item_decoder_MLP])
     model = Model(input=[user_input, item_input],
                   output=[predict_result, user_cost, item_cost])
